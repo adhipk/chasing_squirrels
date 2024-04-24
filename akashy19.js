55f29c641f846aee773db19d46fd8289151de4f0
adhipk <adhip.kashyap@gmail.com>
2024-04-23 20:04:08 -0700

    revert to using circles

--- akashy19.js
+++ akashy19.js
@@ -4,13 +4,13 @@ const margin = 40;
 let width = window.innerWidth - margin;
 let height = window.innerHeight - margin;
 async function drawMap() {
-    // load data check if squirrels approach or runs from or are indifferent, pick 100 random squirrels
+    // load data check if squirrels approach or runs from or are indifferent, pick 100 random squirrels`
     data = await d3.csv(filename).then(data=>data.filter(d=>{
         return d['Approaches'] == "true" || d['Runs from'] == "true" || d['Indifferent'] == "true";
     }).sort(() => 0.5 - Math.random()).splice(0,100));
-
+    console.log(data);
     await d3.json("data/centralPark.geojson").then(function (centralPark) {
-        console.log(centralPark);
+
         // Define projection
         var projection = d3.geoIdentity().reflectY(true).fitSize([width - 20, height - 20], centralPark);
 
@@ -18,19 +18,13 @@ async function drawMap() {
         var path = d3.geoPath().projection(projection);
         const svg = d3.select("#squirrels");
         // Render map
-       
-        const mapGroup = svg.append("g").attr("class", "map-paths");
-      
-        d3.select("#squirrels").selectAll(".map-path")
+        svg.selectAll("path")
             .data(centralPark.features)
-            .append("path")
+            .enter().append("path")
             .attr("d", path)
-            .attr("class", "map-path")
             .attr("fill", "#a7f3d0")
             .attr("stroke", "black");
-        
-        console.log(mapGroup)
-        
+
         // Create force simulation
         const simulation = d3.forceSimulation(data)
             .force("x", d3.forceX().x(d => projection([d.X, d.Y])[0]))
@@ -38,25 +32,19 @@ async function drawMap() {
             // .force("collide", d3.forceCollide(10).strength(1))
             .on("tick", ticked);
         // render points
-        const glyphs = svg.append("g").attr("class", "glyphs");
-        glyphs.selectAll(".glyph")
+        const circles = svg.selectAll("circle")
             .data(data)
-            .enter().append("use")
-            .attr("class", "glyph")
-            .attr("xlink:href", d=>{
-                return `#squirrel-${d['Primary Fur Color']}`;
-            }) // Use the custom SVG glyph
-            .attr("width", 20)
-            .attr("height", 20)
-            .attr("x", d => projection([d.X, d.Y])[0])
-            .attr("y", d => projection([d.X, d.Y])[1]);
+            .enter().append("circle")
+            .attr("cx", function (d) { return projection([d.X, d.Y])[0]; })
+            .attr("cy", function (d) { return projection([d.X, d.Y])[1]; })
+            .attr("r", 5)
+            .attr("fill", "red");
         // 
         // Function to update circle positions
         function ticked() {
-            glyphs
-                .data(data)
-                .attr("x", d => d.x)
-                .attr("y", d => d.y);
+            circles
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
         }
         svg.on("mousemove", function (event) {
             const mouseX = event.offsetX;
@@ -70,14 +58,13 @@ async function drawMap() {
                 else if(d['Runs from'] == "true"){
                     forceDirection = -1;
                 }
-                const dx = 1 * ( mouseX - d.x);
-                const dy = 1 * (mouseY - d.y);
+                const dx = forceDirection * ( mouseX - d.x);
+                const dy = forceDirection * (mouseY - d.y);
                 const distance = Math.sqrt(dx * dx + dy * dy);
                 if ( distance>0 && distance < 20) {
                     const angle = Math.atan2(dy, dx);
                     d.x += Math.cos(angle) * 2;
                     d.y += Math.sin(angle) * 2;
-                    console.log(d.x, d.y);
                 }
             });
 
